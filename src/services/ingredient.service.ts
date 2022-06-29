b5287c7ec7e307e2335f0d55efb62e544ac92f8f
Test Automation <automation.testing.tm@gmail.com>
2022-06-29 21:11:29 +0300

    remove logs

--- src/services/ingredient.service.ts
+++ src/services/ingredient.service.ts
@@ -3,8 +3,6 @@ import { Ingredient } from '../models/ingridient';
 export class IngredientService {
 
     async getIngredientsByIds(ids: string[]) {
-        console.log((await Ingredient.find({}, {name: 1}).lean()).map(el => el.name.toLowerCase()).sort((a:string, b:string) => b.length - a.length))
-        console.log((await Ingredient.find({}, {name: 1}).lean()).map(el => el.name.toLowerCase()).sort((a:string, b:string) => b.length - a.length).slice(98))
         return Ingredient.find({ ingredient_id: { $in: ids } }).lean();
     }
 
