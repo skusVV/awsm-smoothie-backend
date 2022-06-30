16f6de16dd4ae874fcac9b801aa1b91cdf5bbf4f
vskus <vskus@softserveinc.com>
2022-06-30 16:49:54 +0300

    dump update v5

--- src/constants/main-categories.ts
+++ src/constants/main-categories.ts
@@ -1,8 +1,8 @@
 export const mainCategories = [
-    { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/1.png', title: 'Post Workout Shakes', labelText: 'HOT', labelColor: 'yellow', category_id: 'post-workout-shake' },
+    { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/1.png', title: 'Post Workout Shakes', labelText: 'HOT', labelColor: 'yellow', category_id: 'post-workout-protein-shake' },
     { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/2.png', title: 'Meal Replacements Shakes', labelText: 'NEW', labelColor: 'red', category_id: 'meal-replacement-shakes' },
     { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/3.png', title: 'Weight Loss Shakes', labelText: 'YUMMY!', labelColor: 'pink', category_id: 'best-protein-shakes-for-weight-loss' },
-    { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/4.png', title: 'Muscle Gain Shakes', labelText: 'SOOO GOOD!', labelColor: 'blue', category_id: 'muscle-gain-shakes' },
+    { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/4.png', title: 'Muscle Gain Shakes', labelText: 'SOOO GOOD!', labelColor: 'blue', category_id: 'best-protein-powder-for-muscle-gain' },
     { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/5.png', title: 'High Protein Shakes', labelText: 'OMG!', labelColor: 'purple', category_id: 'high-calorie-protein-shakes' },
     { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/6.png', title: 'Low Carb Shakes', labelText: 'DELICIOUS!', labelColor: 'light-blue', category_id: 'low-carb-protein-shakes' },
     { imgUrl: 'https://storage.googleapis.com/smoothie_bucket/categories/7.png', title: 'Keto Shakes', labelText: 'HEALTHY', labelColor: 'green', category_id: 'keto-shakes' },
